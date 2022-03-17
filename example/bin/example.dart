6ead60f80e95056a982a7fc65c6bb90f91c07386
Yakov Karpov <yakovkarpoff@gmail.com>
2022-03-17 03:34:31 +0300

    Fix DCM integration in the corresponding package

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,3 +1,16 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main(List<String> arguments) {}
+
+void triggersLinter() {
+  print("No prints and no double quotes please!");
 }
+
+void triggersDartCodeMetrics(
+  String amount,
+  String of,
+  String arguments,
+  String inThis,
+  String function,
+  String isAbsolutely,
+  String too,
+  String much,
+) {}
