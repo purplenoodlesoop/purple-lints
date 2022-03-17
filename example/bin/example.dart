74ecdc6e529d7780b32355fc19db5fe246f6096e
Yakov Karpov <yakovkarpoff@gmail.com>
2022-03-17 03:43:03 +0300

    Bugfixes and example (#1)
    * Add example
    
    * Remove unusable linter rules
    
    * Fix DCM integration in the corresponding package
    
    * Prepare purle_lints for publishing
    
    * Prepare purle_metrics_lints for publishing

---
+++ example/bin/example.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {}
+
+void triggersLinter() {
+  print("No prints and no double quotes please!");
+}
+
+void triggersDartCodeMetrics(
+  String amount,
+  String of,
+  String arguments,
+  String inThis,
+  String function,
+  String isAbsolutely,
+  String too,
+  String much,
+) {}
--- packages/purple_lints/CHANGELOG.md
+++ packages/purple_lints/CHANGELOG.md
@@ -1,3 +1,10 @@
+## 0.1.1 - [17.03.2022]
+
+### Fixed
+
+- Remove three experimental rules that are not yet usable.
+
+
 ## 0.1.0 - [13.03.2022]
 
 - Initial version.
